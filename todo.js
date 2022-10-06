6f80c441f56c58a9874a892602a77806d58a9243
bugra.arslan <bugra.arslan@gtsfly.de>
2022-10-06 19:16:09 +0300

    update

--- todo.js
+++ todo.js
@@ -5,27 +5,26 @@ const todoList = document.querySelector(".list-group");
 const firstCardBody =document.querySelectorAll(".card-body")[0];
 const secondCardBody =document.querySelectorAll(".card-body")[1];
 
-const filter =document.querySelector("#filter"); // Filtre-Arama Kutusu
-const clearButton=document.querySelector("#clear-todos");// Silme butonu
+const filter =document.querySelector("#filter"); 
+const clearButton=document.querySelector("#clear-todos");
 
 eventListeners();
 
 function eventListeners(){ 
-    form.addEventListener("submit",addTodo); //Form submit olduğunda addTodo fonksiyonunu çalıştır.
-    document.addEventListener("DOMContentLoaded",loadedAllTodosToUI); //DOMContentLoaded sayfa yükledindiğinde loadedAll... fonksiyonunu çalıştıracak.
-    secondCardBody.addEventListener("click",deleteTodo); //cardBody'ye tıklandığında fonksiyonu çalıştır.
-    filter.addEventListener("keyup",filterTodos); //filter kısmına tuşa basıp çekildeğinde filterTodos çalışacak.
-    clearButton.addEventListener("click",clearAllTodos); //Tümünü sil butonu için click eventi ekledik -> clearAllTodos
+    form.addEventListener("submit",addTodo); 
+    document.addEventListener("DOMContentLoaded",loadedAllTodosToUI); 
+    secondCardBody.addEventListener("click",deleteTodo); 
+    filter.addEventListener("keyup",filterTodos); 
+    clearButton.addEventListener("click",clearAllTodos); 
 }
 
 function addTodo(e){
-    let todos = getTodosFromStorage();//kıysalamak için storageden bilgi getirdik
-    const newTodo = todoInput.value.trim() //todoInput alanına girilen değeri TRIM'le ve newTodo ya ata.
-    //addTodoToUI Kısmında en sonda todoInput DEĞERİNİ sıfırladık Alt kısımlara bakabilirsin.
+    let todos = getTodosFromStorage();
+    const newTodo = todoInput.value.trim() 
     if(newTodo === ""){
         showAlert("danger","Lütfen bir to-do giriniz.");
     }
-    else if(todos.indexOf(newTodo)===-1){ //eğer aynısı yoksa ekleyecek
+    else if(todos.indexOf(newTodo)===-1){ 
         addTodoToUI(newTodo);  
         addTodoToStorage(newTodo);
         showAlert("success","Başarılı bir şekilde eklendi...")  
@@ -33,14 +32,14 @@ function addTodo(e){
     else{
         showAlert("warning","Bu to-do zaten kayıtlı");
     }
-    e.preventDefault(); //prevent fonksiyonu ile sayfanın submit olduğunda yenilenmesinin önüne geçtik. 
+    e.preventDefault(); 
 }
 
 function showAlert(type,message){
-    const alert= document.createElement("div");     //div oluşturup buna class ve message-textContent ekledik.
+    const alert= document.createElement("div");     
     alert.className=`alert alert-${type}`;
     alert.textContent=`${message}`;
-    firstCardBody.appendChild(alert);   // karta alerti ekledik.
+    firstCardBody.appendChild(alert);  
 
     setTimeout(function(){
         alert.remove();        
@@ -48,16 +47,15 @@ function showAlert(type,message){
 }
 
 function filterTodos(e){
-     const filterValue= e.target.value.toLowerCase();//filtreleme kutusuna girilen değerleri küçük olarak al (ararken büyük küçük sorununun önüne geçmek için)
-     const listItems=document.querySelectorAll(".list-group-item"); // classı bu olanları getir yani LI elementi
+     const filterValue= e.target.value.toLowerCase();
+     const listItems=document.querySelectorAll(".list-group-item"); 
 
-     listItems.forEach(function(listItem){ //tüm li'lerde gez 
+     listItems.forEach(function(listItem){ 
         const text = listItem.textContent.toLowerCase();
 
         if(text.indexOf(filterValue) === -1){
             //BULAMADI
-            listItem.setAttribute("style","display : none !important"); /* setAttribute ile style verdik display:none yani görünmez yaptık.
-            Ancak BOOTSTRAP kullandığımızdan dolayı d-flex block important kaldığından ekrandan gitmiyor. none !important yaparak çözüyoruz.*/
+            listItem.setAttribute("style","display : none !important"); 
         }
         else listItem.setAttribute("style","display:block");
 
@@ -65,10 +63,10 @@ function filterTodos(e){
 }
 
 
-function deleteTodo(e){ //fonksiyon a(linkin içerisindeki i elementine tıklandığında, class adıyla bularak ve üst evebeynlerini silerek çalışıyor.)
+function deleteTodo(e){ 
       if(e.target.className=== "fa fa-remove"){
         e.target.parentElement.parentElement.remove();
-        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent); // Storageden silme fonksiyonuna hangisine tıkladığına dair bilgi yolluyoruz. (deletetodo)
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent); 
         showAlert("success","To-do başarıyla silindi");
     }
 }
@@ -76,7 +74,7 @@ function deleteTodoFromStorage(deletetodo){
     let todos = getTodosFromStorage();
     todos.forEach(function(todo,index){
         if(todo === deletetodo){
-            todos.splice(index,1); //arrayden değeri sildik
+            todos.splice(index,1); 
         }
     })
     localStorage.setItem("todos",JSON.stringify(todos));
@@ -84,7 +82,7 @@ function deleteTodoFromStorage(deletetodo){
 
 function clearAllTodos(){
     if(confirm("Tüm to-do'ları silmek istiyorsanız onaylayınız !")){
-        //Arayüzden temizleme --- todoList.innerHTML=""; --- ile temizleyebilirdik ancak performans açısından tercih edilmez.
+        
         while(todoList.firstElementChild != null){
             todoList.removeChild(todoList.firstElementChild);
         }
@@ -92,48 +90,40 @@ function clearAllTodos(){
     }
 }
 
-function loadedAllTodosToUI(){ //daha önce yazılan getTodosFromStorage ile todos içine localdekileri atadık.
+function loadedAllTodosToUI(){ 
     let todos=getTodosFromStorage();
-    todos.forEach(function(e){// Todos'daki her eleman için daha önce yazdığımız fonksiyon ile arayüze aktarıyoruz.
+    todos.forEach(function(e){
         addTodoToUI(e);
     })
 }
 
-function getTodosFromStorage(){ //todos var mı diye kontrol ediyor ve yoksa oluşturuyoruz.
+function getTodosFromStorage(){ 
     let todos;
     if(localStorage.getItem("todos")===null){
-        todos=[]; //Boş todos oluşturduk localde 
+        todos=[]; 
     }
     else
     todos=JSON.parse(localStorage.getItem("todos"));
     return todos;
 }
-function addTodoToStorage(newTodo){ //todos'a ekleme yapıyoruz.
+function addTodoToStorage(newTodo){ 
     let todos=getTodosFromStorage();
-    todos.push(newTodo); //boş todos'a newTodo value olarak ekledik.
+    todos.push(newTodo); 
     localStorage.setItem("todos",JSON.stringify(todos));
 }
 
 
-function addTodoToUI(newTodo){//Gelen Değeri "li" şeklinde UI ekleyecek.
+function addTodoToUI(newTodo){
 
-    const listItem=document.createElement("li");    // li elementi oluşturuduk.
-    listItem.className="list-group-item d-flex justify-content-between";    // li elementimize özellikler class vs. verdik.
-    const link=document.createElement("a");     // oluşturulan li elementinin daha sonra içine koymak üzere a elementi oluşturduk.
-    link.href="#"; link.className="delete-item"; link.innerHTML="<i class = 'fa fa-remove'></i>";   // a elementimize özellikler class vs. verdik.
+    const listItem=document.createElement("li");    
+    listItem.className="list-group-item d-flex justify-content-between";    
+    const link=document.createElement("a");     
+    link.href="#"; link.className="delete-item"; link.innerHTML="<i class = 'fa fa-remove'></i>";   
 
-    listItem.appendChild(document.createTextNode(newTodo));     // li'ye newTodo'yu çocuğu olarak ekledik. 
-    listItem.appendChild(link);     // li'ye a'yı ekledik.
+    listItem.appendChild(document.createTextNode(newTodo));     
+    listItem.appendChild(link);     
 
-    todoList.appendChild(listItem);     //todoList'e listItem'ı ekledik.
-    todoInput.value="";     // Arayüze Todo ekleme işlemi bittikten sonra TODO girme çubuğundaki değeri sıfırlamak için "BOŞ DEĞER" atadık. 
+    todoList.appendChild(listItem);     
+    todoInput.value="";      
 
 }
-
-
-
-/*
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
-*/
